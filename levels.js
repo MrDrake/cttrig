e83c590ba83954ae68be597d24f6a2521c44397a
John Drake <johndrake@Deploys-MacBook-Pro-3.local>
2019-07-24 12:39:28 +1000

    fixed sprite rotation and updated style

--- levels.js
+++ levels.js
@@ -1,9 +1,13 @@
-level1 = { name: "Level 1",
-    variables: {bearing: 0, distance: 0},
+level1 = { name: "Level 1 &ndash; Basic Bearings",
+    variables: {bearing:0, distance:0, x:0, y:0},
     init: function() {
-        this.variables.bearing = Math.floor(Math.random()*16)*5 + 5;
-        this.variables.distance = Math.floor(Math.random()*12)*5 + 60;
+        this.variables.bearing = Math.floor(Math.random()*32)*5 + 10;
+        this.variables.distance = Math.floor(Math.random()*20)*5 + 100;
         turtle.a = 90;
+        turtle.x = -50;
+        this.variables.x = this.variables.distance * Math.cos((90-this.variables.bearing)*Math.PI/180) + turtle.x;
+        this.variables.y = this.variables.distance * Math.sin((90-this.variables.bearing)*Math.PI/180) + turtle.y;
+        linegroup.image('blue.png', 32, 32).center(this.variables.x + svgwidth/2, -this.variables.y + svgheight/2);
     },
     text: function() {
         return "<p>You are currently facing due North.  Your friend has a bearing of "
@@ -11,9 +15,7 @@ level1 = { name: "Level 1",
             + "<p>How do you move to your friend?</p>"
     },
     success: function() {
-        var x = this.variables.distance * Math.cos((90-this.variables.bearing)*Math.PI/180);
-        var y = this.variables.distance * Math.sin((90-this.variables.bearing)*Math.PI/180);
-        return Math.abs(turtle.x-x) < 0.01 && Math.abs(turtle.y-y) < 0.01;
+        return Math.abs(turtle.x-this.variables.x) < 0.01 && Math.abs(turtle.y-this.variables.y) < 0.01;
     }
 };
 
--- turtle.js
+++ turtle.js
@@ -2,6 +2,7 @@
 var svgwidth = 500;
 var svgheight = 500;
 var speed = 0.2;
+var colour = 'orange';
 
 // for accessing variables
 Blockly.JavaScript.addReservedWords('code');
@@ -10,16 +11,19 @@ Blockly.JavaScript.addReservedWords('code');
 var draw = SVG('turtleDiv').size(svgwidth, svgheight);
 var linegroup = draw.group();
 
+// current level
+var currentlevel = level1;
+
 // setup turtle object
 var turtle;
 ClearTurtle();
 
 // add buttons on svg
 draw.image('play.png', 25, 25)
-    .move(svgwidth-80, svgheight-25).opacity(0.5)
+    .move(svgwidth-60, svgheight-35).opacity(0.5)
     .click(function() {RunCode()});
 draw.image('reset.png', 25, 25)
-    .move(svgwidth-55, svgheight-25).opacity(0.5)
+    .move(svgwidth-35, svgheight-35).opacity(0.5)
     .click(function() {ClearTurtle()});
 /*draw.image('save.png', 25, 25)
     .move(svgwidth-25, svgheight-25).opacity(0.5)
@@ -49,7 +53,7 @@ function RunCode() {
 
         // show feedback
         // todo: do we always need feedback?
-        if(level1.success()) {  // todo: change to whatever level
+        if(currentlevel && currentlevel.success()) {  // todo: change to whatever level
             alert("Well done!\nYou did it!");   
         } else {
             alert("Not quite!\nTry again!");
@@ -73,33 +77,41 @@ function ClearTurtle() {
 
     // set up turtle object
     turtle = {
-        x: 0,
-        y: 0,
-        a: 0,
-        d: 0,
-        p: true,
-        s: null
+        x: 0, y: 0, a: 0,   // position and orientation
+        d: 0,               // delay
+        p: true,            // is pen down?
+        n: null, s: null    // sprites
     }   
 
     // initialise level and update text
-    level1.init();
-    document.getElementById('instructions').innerHTML = level1.text();
+    currentlevel.init();
+    document.getElementById('leveltitle').innerHTML = currentlevel.name;
+    document.getElementById('instructions').innerHTML = currentlevel.text();
 
     // draw turtle sprite
     var x1 = turtle.x + svgwidth/2;
     var y1 = -turtle.y + svgheight/2;
-    turtle.s = linegroup.image('red.png', 32, 32).center(x1, y1).rotate(90-turtle.a, x1, y1);
+
+    turtle.n = linegroup.nested().center(x1, y1);
+    turtle.s = turtle.n.image('red.png', 32, 32).center(0, 0).rotate(90-turtle.a);
 }
 
 // turn turtle clockwise angle and animate sprite
 function TurtleTurn(angle) {
     turtle.a -= angle;  // svg uses anti-clockwise but we want clockwise
+    if(turtle.a < -180) turtle.a += 360;
+    if(turtle.a > 180) turtle.a -= 360;
+
     var delay = Math.abs(angle) / speed;    // calculate delay for animation
-    var x1 = turtle.x + svgwidth/2;
-    var y1 = -turtle.y + svgheight/2;
-    // animate sprite rotating
-    turtle.s.animate(delay, '<>', 0).rotate(90-turtle.a, turtle.s.cx(), turtle.s.cy());
-    // todo: fix extra rotation of sprite due to multiples of 360
+    
+    // angle to turn sprite
+    var turn = 90-turtle.a;
+    if(turn < -180) turn += 360;
+    if(turn > 180) turn -= 360;
+
+    turtle.s.animate(delay, '<>', turtle.d).rotate(turn);
+    // todo: fix occasional bug of extra rotation of sprite (multiple of 180?)
+
     // add to delay for animation
     turtle.d += delay;
 }
@@ -109,23 +121,27 @@ function TurtleForward(distance) {
     // calculate start of line
     var x1 = turtle.x + svgwidth/2;
     var y1 = -turtle.y + svgheight/2;
+    var dx = distance * Math.cos(turtle.a * Math.PI / 180);
+    var dy = distance * Math.sin(turtle.a * Math.PI / 180);
 
     // calculate end of line
-    turtle.x = turtle.x + distance * Math.cos(turtle.a * Math.PI / 180);
-    turtle.y = turtle.y + distance * Math.sin(turtle.a * Math.PI / 180);
+    turtle.x += dx;
+    turtle.y += dy;
     var x2 = turtle.x + svgwidth/2;
     var y2 = -turtle.y + svgheight/2;
 
     // calculate delay and animate sprite
     var delay = Math.abs(distance) / speed;
-    turtle.d += delay;
-    turtle.s.animate(delay, '<>', 0).dy(-distance);
+    turtle.n.animate(delay, '<>', turtle.d).move(x2, y2);
 
     // only draw path if pen down
     if(turtle.p) {
-        linegroup.line(x1, y1, x1, y1).stroke({ width: 2, linecap: 'round', color: 'orange' })
+        linegroup.line(x1, y1, x1, y1).stroke({ width: 2, linecap: 'round', color: colour})
             .animate(delay, '<>', turtle.d).plot(x1, y1, x2, y2);
     }
+
+    // add to delay for animation
+    turtle.d += delay;
 }
 
 // save code and SVG image
